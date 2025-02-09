50ca211683cf327d00af2bf084b79cff68ccb28b
Reda Hilal <reda0x04@gmail.com>
2025-02-10 02:38:13 +0400

    finish DAL and BLL for Test table

---
+++ DVLD-BusinessLogicLayer/clsTest.cs
@@ -0,0 +1,99 @@
+﻿using DVLD_DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_BusinessLogicLayer
+{
+    public class clsTest
+    {
+        clsGlobalSettings.enMode _Mode = clsGlobalSettings.enMode.AddNew;
+
+        public int ID { get; set; }
+        public int TestAppointmentID { get; set; }
+        public bool Result { get; set; }
+        public string Notes { get; set; }
+        public int CreatedByUserID { get; set; }
+
+        public clsTestAppointment TestAppointment = null;
+        public clsUser User = null;
+
+        public clsTest()
+        {
+            _Mode = clsGlobalSettings.enMode.AddNew;
+            ID = -1;
+            TestAppointmentID = -1;
+            Result = false;
+            Notes = string.Empty;
+            CreatedByUserID = -1;
+        }
+
+        private clsTest(int ID, int TestAppointmentID, bool Result, string Notes, int CreatedByUserID)
+        {
+            _Mode = clsGlobalSettings.enMode.Update;
+
+            this.ID = ID;
+            this.TestAppointmentID = TestAppointmentID;
+            this.Result = Result;
+            this.Notes = Notes;
+            this.CreatedByUserID = CreatedByUserID;
+
+            this.TestAppointment = clsTestAppointment.Find(TestAppointmentID);
+            this.User = clsUser.Find(CreatedByUserID);
+        }
+
+        private bool _AddNewTest()
+        {
+            this.ID = clsTestData.AddNewTest(TestAppointmentID, Result, Notes, CreatedByUserID);
+            return ID != -1;
+        }
+
+        private bool _UpdateTest()
+        {
+            return clsTestData.UpdateTest(ID, TestAppointmentID, Result, Notes, CreatedByUserID);
+        }
+
+        public static clsTest Find(int ID)
+        {
+            int TestAppointmentID = -1;
+            bool Result = false;
+            string Notes = string.Empty;
+            int CreatedByUserID = -1;
+
+            if (clsTestData.GetTestInfo(ID, ref TestAppointmentID, ref Result, ref Notes, ref CreatedByUserID))
+                return new clsTest(ID, TestAppointmentID, Result, Notes, CreatedByUserID);
+            else
+                return null;
+        }
+
+        public bool Save()
+        {
+            switch (_Mode)
+            {
+                case clsGlobalSettings.enMode.AddNew:
+                    _Mode = clsGlobalSettings.enMode.Update;
+                    return _AddNewTest();
+
+                case clsGlobalSettings.enMode.Update:
+                    return _UpdateTest();
+
+                default:
+                    return false;
+            }
+        }
+
+        public static bool DeleteTest(int ID)
+        {
+            return clsTestData.DeleteTest(ID);
+        }
+
+        public static DataTable GetAllTests()
+        {
+            return clsTestData.GetAllTests();
+        }
+    }
+
+}
---
+++ DVLD-DataAccessLayer/clsTestData.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DataAccessLayer
+{
+    public class clsTestData
+    {
+        public static bool GetTestInfo(int ID, ref int TestAppointmentID, ref bool Result, ref string Notes, ref int CreatedByUserID)
+        {
+            bool isFound = false;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"SELECT * FROM [dbo].[Test] WHERE ID = @ID;";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@ID", ID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    isFound = true;
+                    TestAppointmentID = (int)reader["TestAppointmentID"];
+                    Result = (bool)reader["Result"];
+                    Notes = reader["Notes"].ToString();
+                    CreatedByUserID = (int)reader["CreatedByUserID"];
+                }
+                reader.Close();
+            }
+            catch { isFound = false; }
+            finally { connection.Close(); }
+
+            return isFound;
+        }
+
+        public static int AddNewTest(int TestAppointmentID, bool Result, string Notes, int CreatedByUserID)
+        {
+            int ID = -1;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"INSERT INTO [dbo].[Test] 
+                        ([TestAppointmentID], [Result], [Notes], [CreatedByUserID])
+                        VALUES (@TestAppointmentID, @Result, @Notes, @CreatedByUserID);
+                        SELECT SCOPE_IDENTITY();";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@TestAppointmentID", TestAppointmentID);
+            command.Parameters.AddWithValue("@Result", Result);
+            command.Parameters.AddWithValue("@Notes", Notes);
+            command.Parameters.AddWithValue("@CreatedByUserID", CreatedByUserID);
+
+            try
+            {
+                connection.Open();
+                object result = command.ExecuteScalar();
+                if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                {
+                    ID = insertedID;
+                }
+            }
+            catch { }
+            finally { connection.Close(); }
+
+            return ID;
+        }
+
+        public static bool UpdateTest(int ID, int TestAppointmentID, bool Result, string Notes, int CreatedByUserID)
+        {
+            int RowsAffected = 0;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"UPDATE [dbo].[Test]
+                        SET [TestAppointmentID] = @TestAppointmentID,
+                            [Result] = @Result,
+                            [Notes] = @Notes,
+                            [CreatedByUserID] = @CreatedByUserID
+                        WHERE ID = @ID;";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@TestAppointmentID", TestAppointmentID);
+            command.Parameters.AddWithValue("@Result", Result);
+            command.Parameters.AddWithValue("@Notes", Notes);
+            command.Parameters.AddWithValue("@CreatedByUserID", CreatedByUserID);
+            command.Parameters.AddWithValue("@ID", ID);
+
+            try
+            {
+                connection.Open();
+                RowsAffected = command.ExecuteNonQuery();
+            }
+            catch { }
+            finally { connection.Close(); }
+
+            return RowsAffected > 0;
+        }
+
+        public static bool DeleteTest(int ID)
+        {
+            int RowsAffected = 0;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"DELETE FROM [dbo].[Test] WHERE ID = @ID;";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@ID", ID);
+
+            try
+            {
+                connection.Open();
+                RowsAffected = command.ExecuteNonQuery();
+            }
+            catch { return false; }
+            finally { connection.Close(); }
+
+            return RowsAffected > 0;
+        }
+
+        public static DataTable GetAllTests()
+        {
+            DataTable dtTests = new DataTable();
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"SELECT * FROM [dbo].[Test];";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                    dtTests.Load(reader);
+
+                reader.Close();
+            }
+            catch { }
+            finally { connection.Close(); }
+
+            return dtTests;
+        }
+    }
+
+}
